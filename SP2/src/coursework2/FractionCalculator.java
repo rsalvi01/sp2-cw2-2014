ee3b4044405be6508e66ff8e136a1aff0312d4a2
rsalvi01 <renansalviato@ig.com.br>
2014-11-05 22:29:10 +0000

    Delete previous attempted and starting from zero again.

--- SP2/src/coursework2/FractionCalculator.java
+++ SP2/src/coursework2/FractionCalculator.java
@@ -1,70 +1,29 @@
 package coursework2;
 
+import java.util.Scanner;
+
 public class FractionCalculator
-{
-	char bo; //basic operators to be remembered: (*) (/) (+) (-)
-	char oo; //other operations: (A) or (a) absolute value, (N) or (n) negate and (Q) or (q) quit
-	
-	public String validateInput(String inputString)
+{		
+	/*public Fraction evaluate (Fraction input, String inputString)
 	{
-		final String[] BAD_OPERATORS = {"* /","* -","* +","* *","/ *","/ -","/ +","/ /","- *","- /","- +","- -","+ *","+ /","+ -","+ +"};
-		boolean error = false;
 		
-		//replacing any double white spaces
-		//tip on using \s+ came from
-		//http://stackoverflow.com/questions/3958955/how-to-remove-duplicate-white-spaces-in-string-using-java
-		inputString = inputString.replaceAll("\\s+", " ");
-				
-		//checking that input doesn't contain double operator
-		for (int i = 0; i < BAD_OPERATORS.length; i++)
-		{
-			if (error == false)
-			{
-				if (inputString.contains(BAD_OPERATORS[i]))
-				{
-					error = true;
-				}
-			}
-		}		
-		
-		if (error)
-		{
-			return "BadInput";
-		}
-		else
-		{
-			return inputString;		
-		}
-	}
+	}*/
 	
-	public Fraction evaluate (Fraction input, String inputString)
-	{	
-		String[] userInputs;
+	public static void main (String []args)
+	{
+		Scanner in = new Scanner(System.in);
+		String exit = "";
+		String lastValue = "0";
+		String userInput = "";
 		
-		boolean proceed = true;
-		
-		if (validateInput(inputString).equals("BadInput"))
-		{
-			proceed = false;			
-		}
-		
-		if (!proceed)
-		{
-			System.out.print("The input your entered contains double operators (e.g.: the times symbol (*)");
-			System.out.println("followed by the addition symbol (+). Please try again.");
-			return null;
-		}
-		else
-		{
-			//splitting user input into an array				
-			userInputs = inputString.split(" ");
-		}		
-		
-		/*for (int i = 0; i < userInputs.length; i++)
-		{
-			if(userInputs[0])
-		}*/
-		
-		return null;
+		Fraction c = new Fraction(9,3);
+				
+		while (!(exit.equals("q")) && !(exit.equals("Q")))
+			{
+				System.out.println("Welcome to Renan Salviato's Fraction Calculator");
+				System.out.println("The last remembered value is: "+lastValue);
+				System.out.println("Please enter your expression to be evaluated: ");
+				userInput = in.nextLine();
+			}		
 	}
 }
