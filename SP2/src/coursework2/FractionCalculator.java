dbe575eecc5b7796cd8926949ffc70c8df30b031
rsalvi01 <renansalviato@ig.com.br>
2014-11-10 00:15:32 +0000

    Going round in circles...

--- SP2/src/coursework2/FractionCalculator.java
+++ SP2/src/coursework2/FractionCalculator.java
@@ -1,21 +1,25 @@
 package coursework2;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FractionCalculator
 {	
 	public static Fraction evaluate(Fraction frac, String inputString)
 	{
-		ArrayList<String> userInput = Helper.tokenizeInput(inputString,"/");
-		int tokens = userInput.size();
+		String[] userInput = inputString.split(" ");
+		int tokens = userInput.length;
 		String operator = "";
 		String modifier = "";
-		Fraction tempFrac = null; 
+		Fraction tempFrac = null;		
 		
 		for (int i = 0; i < tokens; i++)
 		{
-			String temp = userInput.get(i);
+			String temp = userInput[i];
+			
+			//
+			//First IF block to analyse current token
+			//
+			
 			if (Helper.isOperator(temp))
 			{
 				if (operator == "")
@@ -26,6 +30,7 @@ public class FractionCalculator
 				{
 					System.out.println("Consecutive operators (*, /, + and -) are not accepted.");
 					frac = new Fraction (0,1);
+					i = tokens;
 				}
 			}
 			else if (Helper.isModifier(temp))
@@ -36,9 +41,9 @@ public class FractionCalculator
 			{
 				if(temp.contains("/"))
 				{
-					String[] tempFraction = temp.split("/");
-					int num = Integer.parseInt(tempFraction[0]);
-					int denom = Integer.parseInt(tempFraction[1]);
+					String[] tempString = temp.split("/");
+					int num = Integer.parseInt(tempString[0]);
+					int denom = Integer.parseInt(tempString[1]);
 					tempFrac = new Fraction (num, denom);
 				}
 				else
@@ -46,129 +51,119 @@ public class FractionCalculator
 					int num = Integer.parseInt(temp);
 					tempFrac = new Fraction(num);
 				}
-				
-				if(operator == "")
-				{
-					frac = tempFrac;
-					tempFrac = null;
-				}
-				else if (operator != "" && modifier == "")
-				{									
-					switch (operator)
-					{
-						case "*":	frac = frac.multiply(tempFrac);
-									tempFrac = null;
-									operator = "";
-									break;
-						case "/":	frac = frac.divide(tempFrac);
-									tempFrac = null;
-									operator = "";
-									break;				          		  
-						case "+":	frac = frac.add(tempFrac);
-									tempFrac = null;
-									operator = "";
-									break;
-						case "-":	frac = frac.subtract(tempFrac);
-									tempFrac = null;
-									operator = "";
-									break;
-					 }					
-				}
-				else if (modifier != "")
-				{
-					switch (modifier)
-					{
-						case "A":	frac = tempFrac.absValue();
-									tempFrac = null;
-									break;
-						case "Abs": frac = tempFrac.absValue();
-									tempFrac = null;
-									break;
-						case "a":	frac = tempFrac.absValue();
-									tempFrac = null;
-									break;
-									
-						case "C":	frac = new Fraction (0,1);
-									tempFrac = null;
-									break;
-						case "Clear": frac = tempFrac.absValue();
-									tempFrac = null;
-									break;
-						case "c":	frac = tempFrac.absValue();
-									tempFrac = null;
-									break;
-									
-						case "N":	frac = tempFrac.negate();
-									tempFrac = null;
-									break;
-						case "Neg": frac = tempFrac.negate();
-									tempFrac = null;
-									break;
-						case "n":	frac = tempFrac.negate();
-									tempFrac = null;
-									break;
-						case "Q":	frac = null;
-									tempFrac = null;
-									break;
-						case "Quit": frac = null;
-									tempFrac = null;
-									break;
-						case "q":	frac = null;
-									tempFrac = null;
-									break;
-					}
-				}
 			}
-		}
-		
+			else
+			{				
+				System.out.println("I am sorry but there was an error in your input. Please try again.");
+				return tempFrac;
+			}
+			//End of first IF block
+			
+			
+			//
+			//Second IF Block
+			//
+							
+			//if current token is a modifier, it needs to be applied to the last known fraction, ignoring any operator remembered.
+			if (modifier != "")
+			{
+				switch (modifier)
+				{
+					case "A":	frac = frac.absValue();
+								tempFrac = null;
+								break;
+					case "Abs": frac = frac.absValue();
+								tempFrac = null;
+								break;
+					case "a":	frac = frac.absValue();
+								tempFrac = null;
+								break;
+								
+					case "C":	frac = new Fraction (0,1);
+								tempFrac = null;
+								break;
+					case "Clear": frac = new Fraction (0,1);
+								tempFrac = null;
+								break;
+					case "c":	frac = new Fraction (0,1);
+								tempFrac = null;
+								break;
+								
+					case "N":	frac = frac.negate();
+								tempFrac = null;
+								break;
+					case "Neg": frac = frac.negate();
+								tempFrac = null;
+								break;
+					case "n":	frac = frac.negate();
+								tempFrac = null;
+								break;
+					case "Q":	frac = null;
+								tempFrac = null;
+								break;
+					case "Quit": frac = null;
+								tempFrac = null;
+								break;
+					case "q":	frac = null;
+								tempFrac = null;
+								break;
+				}
+				modifier = ""; //clearing modifier for next iteration
+			}			
+			//if current token is a fraction
+			else if (operator == "") //and operator is blank, then current fraction replaces the previous known fraction
+			{
+				frac = tempFrac;				
+			}
+			else //if operator is not blank
+			{									
+				switch (operator)
+				{
+					case "*":	frac = frac.multiply(tempFrac);
+								tempFrac = null;
+								break;
+					case "/":	frac = frac.divide(tempFrac);
+								tempFrac = null;
+								break;				          		  
+					case "+":	frac = frac.add(tempFrac);
+								tempFrac = null;
+								break;
+					case "-":	frac = frac.subtract(tempFrac);
+								tempFrac = null;
+								break;
+				}				
+			}			
+		}//End of For loop
 		return frac;
-	}
+	}//End of Class
+		
+		
 	
+	
+	//
+	//MAIN METHOD
+	//
 	public static void main (String []args)
 	{
 		Scanner in = new Scanner(System.in);
-		String userInput = "";
-		boolean exit = false;
-		boolean validInput = false;
+		String userInput = "";		
 		Fraction lastFraction = new Fraction (0,1);		
 		
-		do
-		{
-			System.out.println("Welcome to Renan Salviato's Fraction Calculator.");
-			System.out.println();
-			
-			System.out.println("Current value in calculator: "+lastFraction.toString());
-			System.out.println();
-			
-			while (!validInput)
-			{
-				System.out.println("Please input your calculation or type \"Q\" to quit: ");
+		System.out.println("Welcome to Renan Salviato's Fraction Calculator.");
+		System.out.println();
 		
-				userInput = in.nextLine();
-							
-				userInput = Helper.removeDuplicateSpaces(userInput);
+		System.out.println("Current value in calculator: "+lastFraction.toString());
+		System.out.println();
+		
+		System.out.println("Please input your calculation or type \"Q\" to quit: ");
+
+		userInput = in.nextLine();
 					
-				if (!(Helper.isInputValid(userInput)))
-				{
-					System.out.println("Your input contains double operator, please try again.");
-					System.out.println();
-				}
-				else
-				{
-					validInput = true;				
-				}					
-			}
-							
-			lastFraction = evaluate(lastFraction,userInput);
-			
-			if (lastFraction == null)
-			{
-				exit = true;
-				System.out.println("Thank you for using my calculator.");
-			}
-			
-			System.out.println(lastFraction.toString());
-		} while (exit);
+		userInput = Helper.removeDuplicateSpaces(userInput);
+		
+		lastFraction = evaluate(lastFraction,userInput);
+		
+		
 		
 		in.close();
 	}
--- SP2/src/coursework2/Helper.java
+++ SP2/src/coursework2/Helper.java
@@ -1,46 +1,17 @@
 package coursework2;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
 
 public class Helper
 {
+
+	//removing any consecutive white spaces from user input
+	//the tip on using \s+ came from StackOverflow at
+	//http://stackoverflow.com/questions/3958955/how-to-remove-duplicate-white-spaces-in-string-using-java
 	public static String removeDuplicateSpaces(String userInput)
 	{
-		//replacing any sequential white spaces from user input
-		//the tip on using \s+ came from StackOverflow at
-		//http://stackoverflow.com/questions/3958955/how-to-remove-duplicate-white-spaces-in-string-using-java
 		return userInput = userInput.replaceAll("\\s+", " ");
-	}
-
-	public static boolean isInputValid(String userInput)
-	{
-		//checking that input doesn't contain double operator
-		final String[] BAD_OPERATORS = {"* /","* -","* +","* *","/ *","/ -","/ +","/ /","- *","- /","- +","- -","+ *","+ /","+ -","+ +","*/","*-","*+","**","/*","/-","/+","//","-*","-/","-+","--","+*","+/","+-","++"};
-		for (int i = 0; i < BAD_OPERATORS.length; i++)
-		{
-			if (userInput.contains(BAD_OPERATORS[i]))
-			{
-				return false;
-			}
-		}
-		return true;
-	}
-
-	public static ArrayList<String> tokenizeInput(String userInput, String delimiter)
-	{
-		ArrayList<String> listTest = new ArrayList<String>( );
-		StringTokenizer st = new StringTokenizer(userInput, delimiter);
-
-		while (st.hasMoreTokens())
-		{
-			listTest.add(st.nextToken());
-		}
-
-		return listTest;		
-	}
-
+	}	
+	
 	public static boolean isOperator(String token)
 	{
 		String[] operators = {"*","/","+","-"};
